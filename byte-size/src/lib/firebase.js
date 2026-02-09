7a6cae40fd30059238d55aaf059b15cb5585d348
Kenneth Cellona <kenneth.cellona@edu.sait.ca>
2026-02-08 23:55:52 -0700

    Update firebase.js

--- byte-size/src/lib/firebase.js
+++ byte-size/src/lib/firebase.js
@@ -5,12 +5,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1ST99wHU2_gsGG_x8CBbygFqo2iLGV4g",
-  authDomain: "byte-size-dd84f.firebaseapp.com",
-  projectId: "byte-size-dd84f",
-  storageBucket: "byte-size-dd84f.firebasestorage.app",
-  messagingSenderId: "1021402167470",
-  appId: "1:1021402167470:web:767af84cd59f6b409ad53f"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
