bce36e23acf54bf3176a1c5994b09f41b0354044
Cris1037 <128873608+Cris1037@users.noreply.github.com>
2026-02-24 22:48:11 -0700

    update to allow for pull request
    trying to update main so pull request can go through

--- byte-size/src/lib/firebase.js
+++ byte-size/src/lib/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { browserLocalPersistence, getAuth, GoogleAuthProvider, setPersistence } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -18,6 +18,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+setPersistence(auth, browserLocalPersistence);
 
 // Initialize Firebase Auth and Google Provider
 const auth = getAuth(app);
